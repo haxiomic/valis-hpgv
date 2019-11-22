c344ec294593c447d3f9ee9368a5f91cdcf9a55f
Emma O'Neill <emmaruthoneill@gmail.com>
2019-11-22 13:28:47 -0800

    added highlight location to annotation tracks

--- examples/advanced/App.ts
+++ examples/advanced/App.ts
@@ -2,11 +2,9 @@ const GV = require('../../dist/valis-hpgv.js');
 
 // pass a list of files to visualize in an array, the viewer will determine the best visualization to use
 let hpgv = new GV.GenomeVisualizer([
-    // GRCh38 DNA sequence
-    // 'https://s3-us-west-1.amazonaws.com/valis-file-storage/genome-data/GRCh38.vdna-dir',
-    // // GRCh38 genes
-    // 'https://s3-us-west-1.amazonaws.com/valis-file-storage/genome-data/GRCh38.92.vgenes-dir',
-    // // Cerebellum, DNase
+    'https://encoded-build.s3.amazonaws.com/browser/GRCh38/GRCh38.vdna-dir',
+    'https://encoded-build.s3.amazonaws.com/browser/GRCh38/GRCh38.vgenes-dir',
+    'https://www.encodeproject.org/files/ENCFF609BMS/@@download/ENCFF609BMS.bigBed',
     'https://www.encodeproject.org/files/ENCFF833POA/@@download/ENCFF833POA.bigWig',
 ]);
 
